f522c0dae88bf1ce2c0bcc7553fc967e66d44ef6
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-06-03 18:58:47 -0500

    Linting.
    I ran eslint on `main.js` and `script.js` to fix formatting issues.

--- public/script.js
+++ public/script.js
@@ -1,36 +1,36 @@
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search)
 
-let job_items=document.querySelector('#job-items')
-let job_pane=document.querySelector('.right-job-pane')
+const jobItems = document.querySelector('#job-items')
+const jobPane = document.querySelector('.right-job-pane')
 
-let jobs={}
+let jobs = {}
 
-function change(id, key){
-  let value=prompt("hi");
-  jobs[id][key]=value
-  display_job(id);
+function change (id, key) {
+  const value = prompt('hi')
+  jobs[id][key] = value
+  displayJob(id)
 }
 
-function save(){
+function save () {
   (async () => {
     const rawResponse = await fetch('/save', {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(jobs)
-    });
-    const content = await rawResponse.text();
+    })
+    const content = await rawResponse.text()
 
-    console.log(content);
-  })();
+    console.log(content)
+  })()
 }
 
-function display_job(id){
-  window.history.pushState("", "", `/?job=${id}`);
-  let job=jobs[id];
-  let html=`
+function displayJob (id) {
+  window.history.pushState('', '', `/?job=${id}`)
+  const job = jobs[id]
+  const html = `
   <h1 onclick='change("${id}", "title")'>${job.title}</h1>
   <h2 onclick='change("${id}", "company")'>${job.company}</h2>
   <h3 onclick='change("${id}", "date_submitted")'>${job.date_submitted}</h3>
@@ -40,46 +40,46 @@ function display_job(id){
   <p  onclick='change("${id}", "some_other_attribute")'>${job.some_other_attribute}</p>
   <button onclick='save()'>Save</button>
   `
-  job_pane.innerHTML=html;
+  jobPane.innerHTML = html
 }
 
-function addApplication(){
-  let id=btoa(Math.random()).substring(0,12)
-  jobs[id]={
-    'title':'Default Title',
-    'company':'Default Company',
-    'date_submitted':'Default Date',
-    'status':'Default Status'
+function addApplication () {
+  const id = btoa(Math.random()).substring(0, 12)
+  jobs[id] = {
+    title: 'Default Title',
+    company: 'Default Company',
+    date_submitted: 'Default Date',
+    status: 'Default Status'
   }
   refresh()
 }
 
-function refresh(){
-  job_items.innerHTML='';
-  for(let job_id in jobs){
-    job=jobs[job_id]
-    let d=document.createElement("div");
-    let html=`
-<div class=job-items-row onclick='display_job("${job_id}")'>
+function refresh () {
+  jobItems.innerHTML = ''
+  for (const jobId in jobs) {
+    const job = jobs[jobId]
+    const d = document.createElement('div')
+    const html = `
+<div class=job-items-row onclick='displayJob("${jobId}")'>
   <div>${job.title}</div>
   <div>${job.company}</div>
   <div>${job.date_submitted}</div>
   <div>${job.status}</div>
 </div>`
-    d.innerHTML=html;
-    job_items.appendChild(d);
+    d.innerHTML = html
+    jobItems.appendChild(d)
   }
 
-  let d=document.createElement("div");
-  d.innerHTML=`<button>Add Application</button>`
-  d.onclick=addApplication
-  job_items.appendChild(d);
-  display_job(urlParams.get('job'))
+  const d = document.createElement('div')
+  d.innerHTML = '<button>Add Application</button>'
+  d.onclick = addApplication
+  jobItems.appendChild(d)
+  displayJob(urlParams.get('job'))
 }
 
 fetch('/jobs.json')
   .then(x => x.json())
   .then(y => {
-    jobs=y;
+    jobs = y
     refresh()
-  });
+  })
